27e0929c0e829cf04369563c82c01e6befe95849
subarnapkrl <subarnapokharel252@gmail.com>
2023-05-19 05:26:39 +0545

    Tab-switching-Mechanism

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ import { initialPageLoad } from './initial-page-load.js';
 import { menu } from './menu.js';
 import { contact } from './contact.js';
 
-// initialPageLoad();
-// menu();
- contact();
+initialPageLoad();
+function switchTabs(){
+
+const homeLink=document.querySelector('#home');
+homeLink.addEventListener('click',()=>{
+    clearCOntent()
+    initialPageLoad();
+})
+
+
+
+  const menuLink=document.querySelector('#menu');
+ menuLink.addEventListener('click',()=>{
+    clearCOntent()
+    menu();
+ })
+
+ const contactLink=document.querySelector('#contact');
+ contactLink.addEventListener('click',()=>{
+    clearCOntent()
+    contact();
+ })
+
+}
+
+function clearCOntent()
+{
+   const content=document.querySelector('#content');
+   const nav=document.querySelector('nav');
+   const main=document.querySelector('main');
+    if(nav && main){
+        content.removeChild(nav)
+        content.removeChild(main)
+    }
+   
+   
+}
+switchTabs()
+
