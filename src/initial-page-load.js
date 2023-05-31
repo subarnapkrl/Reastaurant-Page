6410f7c6461face462ba33882271cceeed93cc4f
Soobarna <72292918+subarnapkrl@users.noreply.github.com>
2023-06-01 05:44:30 +0545

    Update initial-page-load.js

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -12,25 +12,7 @@ function initialPageLoad()
     backgroundDiv.classList.add('background');
     main.append(backgroundDiv);
 
-    const header=document.createElement('h1');
-    const spanHead1=document.createElement('span');
-    spanHead1.classList.add('span');
-    spanHead1.textContent="Delivering";
-
-    const spanHead2=document.createElement('span');
-    spanHead2.classList.add('span');
-    spanHead2.textContent="Happiness";
-
-    header.append(spanHead1);
-    header.append(spanHead2);
-
-    main.append(header);
-
-    const button=document.createElement('button');
-    button.classList.add('btn');
-    button.textContent="Order Now";
-    main.append(button);
-
+ 
     //END MAIN SECTION
 
 
